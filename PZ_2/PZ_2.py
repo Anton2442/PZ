2828341f8e91148036e155a819bb0c07f35e1725
Anton2442 <tutuponchick@gmail.com>
2023-11-02 19:27:38 +0300

    Added file PZ_4.pdf Modified files PZ_2.py and PZ_3_1.py

--- PZ_2/PZ_2.py
+++ PZ_2/PZ_2.py
@@ -1,5 +1,20 @@
 # Вывести трёхзначное число справа налево.
 
+check = False
 
-num = input("Введите трёхзначное число: ")
-print(num[::-1])
+while check == False:
+    num = input("Введите трёхзначное число: ")
+    try:
+        while  not (int(num) // 100 < 10) or not (int(num) // 100 >= 1):
+            num = input("Введите трёхзначное число: ")
+        check = True
+        num = int(num)
+    except:
+        print("Число должно быть целым!")
+
+
+a = num % 10
+b = (num // 10) % 10
+c = num // 100
+
+print(a, b, c, sep="")
